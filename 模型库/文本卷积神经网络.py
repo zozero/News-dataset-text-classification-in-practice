c039e9b49bcf829a10accdb2f662545c0fe81e11
zozero <929096507@qq.com>
2022-04-16 19:45:05 +0800

    1、添加文本卷积神经网络； 2、修改部分命名；

--- 模型库/文本卷积神经网络.py
+++ 模型库/文本卷积神经网络.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 class 配置:
     def __init__(self, 数据文件夹, 字向量文件名, 字嵌入方式):
-        self.模型名 = '文本循环神经网络'
+        self.模型名 = '文本卷积神经网络'
         self.训练集路径 = 数据文件夹 + '/数据仓/训练集.txt'
         self.测试集路径 = 数据文件夹 + '/数据仓/测试集.txt'
         self.验证集路径 = 数据文件夹 + '/数据仓/验证集.txt'
@@ -27,25 +27,32 @@ class 配置:
         self.句子长度 = 32
         self.学习率 = 1e-3
         self.字向量长度 = self.预处理的字向量.size(1) if self.预处理的字向量 is not None else 300
-        self.单隐层的长度 = 128
-        self.字关联的深度 = 2  # 可能是字与字之间相互关联的深度，就是当前字要关联到之前的第几个字
+        self.卷积核尺寸 = (2, 3, 4)
+        self.卷积核个数 = 256
 
 
 class 神经网络模型(nn.Module):
     def __init__(self, 模型配置):
         super(神经网络模型, self).__init__()
-
         if 模型配置.预处理的字向量 is not None:
             self.文字张量 = nn.Embedding.from_pretrained(模型配置.预处理的字向量, freeze=False)
         else:
             self.文字张量 = nn.Embedding(模型配置.文字编码字典行数, 模型配置.字向量长度, padding_idx=模型配置.文字编码字典行数 - 1)
-        self.长短期记忆 = nn.LSTM(模型配置.字向量长度, 模型配置.单隐层的长度, 模型配置.字关联的深度, bidirectional=True, batch_first=True,
-                             dropout=模型配置.失活概率)
-        self.全连接层 = nn.Linear(模型配置.单隐层的长度 * 2, 模型配置.类别数)
+
+        self.卷积神经网络 = nn.ModuleList([nn.Conv2d(1, 模型配置.卷积核个数, (尺寸, 模型配置.字向量长度)) for 尺寸 in 模型配置.卷积核尺寸])
+        self.失活概率 = nn.Dropout(模型配置.失活概率)
+        self.全连接层 = nn.Linear(模型配置.卷积核个数 * len(模型配置.卷积核尺寸), 模型配置.类别数)
+
+    def 卷积和池化(self, x, 卷积):
+        x = F.relu(卷积(x)).squeeze(3)
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        return x
 
     def forward(self, x):
-        x, _ = x
-        输出 = self.文字张量(x)
-        输出, _ = self.长短期记忆(输出)
-        输出 = self.全连接层(输出[:, -1, :])
+        输出 = self.文字张量(x[0])
+        输出 = 输出.unsqueeze(1)
+        输出 = torch.cat([self.卷积和池化(输出, 卷积) for 卷积 in self.卷积神经网络], 1)
+        输出 = self.失活概率(输出)
+        输出 = self.全连接层(输出)
+
         return 输出
